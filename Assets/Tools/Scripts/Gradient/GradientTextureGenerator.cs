7b0a7e4d3b7c00c4492a0e713ede57913e90ce69
Oleksandr Sierovoi <alexander.serovoy@gmail.com>
2020-12-05 17:35:52 +0200

    Added ui tools

---
+++ Assets/Tools/Scripts/Gradient/GradientTextureGenerator.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace alexnown
+{
+    [ExecuteInEditMode]
+    public class GradientTextureGenerator : MonoBehaviour
+    {
+        private const string DefaultShaderName = "Unlit/BlendWithRotatable";
+
+        public Gradient Gradient;
+        [Range(0, 360)]
+        public int Angle;
+        [Range(64, 2048)]
+        public int TextureWidth = 256;
+        public TextureWrapMode WrapMode = TextureWrapMode.Clamp;
+        [Header("Material settings")]
+        public string TextureFieldName = "_SecondTex";
+        public string AngleFieldName = "_AngleDeg";
+        [Header("Target graphic")]
+        public GameObject GraphicGo = null;
+        public bool CreateMaterialByShader = true;
+        public Shader TargetShader = null;
+
+        private Material _material;
+
+
+        public Texture2D GenerateTexture(int width)
+        {
+            var texture = new Texture2D(width, 1, TextureFormat.ARGB32, false);
+            texture.wrapMode = WrapMode;
+            for (int i = 0; i < width; i++)
+            {
+                var color = Gradient.Evaluate((float)i / width);
+                texture.SetPixel(i, 0, color);
+            }
+            texture.Apply(false);
+            return texture;
+        }
+
+        private void Start()
+        {
+            InitializeTargetMaterial();
+            UpdateMaterial();
+        }
+
+        private void InitializeTargetMaterial()
+        {
+            if (CreateMaterialByShader)
+            {
+                _material = new Material(TargetShader);
+                SetMaterialToTargetGo(GraphicGo, _material);
+            }
+            else
+            {
+                _material = GetMaterialFromGraphicGo(GraphicGo);
+                _material = Instantiate(_material);
+                SetMaterialToTargetGo(GraphicGo, _material);
+            }
+        }
+
+        private void UpdateMaterial()
+        {
+            if (_material == null) return;
+            _material.SetFloat(AngleFieldName, Angle);
+            _material.SetTexture(TextureFieldName, GenerateTexture(TextureWidth));
+        }
+
+        private Material GetMaterialFromGraphicGo(GameObject graphicGo)
+        {
+            if (graphicGo == null) return null;
+            var graphic = graphicGo.GetComponent<Graphic>();
+            if (graphic != null) return graphic.defaultMaterial;
+            var rend = graphicGo.GetComponent<Renderer>();
+            if (rend != null) return rend.sharedMaterial;
+            return null;
+        }
+
+        private void SetMaterialToTargetGo(GameObject graphicGo, Material mat)
+        {
+            if (graphicGo == null) return;
+            var graphic = graphicGo.GetComponent<Graphic>();
+            if (graphic != null) graphic.material = mat;
+            var rend = graphicGo.GetComponent<Renderer>();
+            if (rend != null) rend.material = mat;
+        }
+
+#if UNITY_EDITOR
+        private void OnDestroy()
+        {
+            if (!Application.isPlaying && CreateMaterialByShader && _material != null)
+            {
+                SetMaterialToTargetGo(GraphicGo, null);
+                DestroyImmediate(_material);
+            }
+        }
+
+        protected void OnValidate()
+        {
+            if (GraphicGo == null)
+            {
+                if (gameObject.GetComponent<Graphic>() != null || gameObject.GetComponent<Renderer>() != null)
+                    GraphicGo = gameObject;
+            }
+            if (CreateMaterialByShader && TargetShader == null)
+                TargetShader = Shader.Find(DefaultShaderName);
+            if (Gradient == null)
+            {
+                Gradient = new Gradient();
+                Gradient.SetKeys(new[]
+                {
+                    new GradientColorKey(new Color32(36,145,214,255), 0),
+                    new GradientColorKey(new Color32(36, 211,214,255), 1)
+                },
+                new[] { new GradientAlphaKey(1, 0), new GradientAlphaKey(1, 1) });
+            }
+            InitializeTargetMaterial();
+            UpdateMaterial();
+        }
+#endif
+    }
+}
---
+++ Assets/Tools/Shaders/Unlit/BlendWithRotatable.shader
@@ -0,0 +1,73 @@
+﻿Shader "Unlit/BlendWithRotatable"
+{
+	Properties
+	{
+		_MainTex("Texture", 2D) = "white" {}
+		_SecondTex("SecondTex", 2D) = "white" {}
+		_AngleDeg("Angle", Range(0,360)) = 0
+	}
+		SubShader
+		{
+			Tags { "Queue" = "Transparent" }
+			LOD 100
+			Blend SrcAlpha OneMinusSrcAlpha
+
+			Pass
+			{
+				CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+				#include "UnityCG.cginc"
+
+				struct appdata
+				{
+					float4 vertex : POSITION;
+					float2 uv : TEXCOORD0;
+				};
+
+				struct v2f
+				{
+					float2 uv : TEXCOORD0;
+					float4 vertex : SV_POSITION;
+				};
+
+				sampler2D _MainTex;
+				sampler2D _SecondTex;
+				float4 _MainTex_ST;
+				float _AngleDeg;
+
+				v2f vert(appdata v)
+				{
+					v2f o;
+					o.vertex = UnityObjectToClipPos(v.vertex);
+					o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+					return o;
+				}
+
+				float2 Unity_Rotate_Degrees_float(float2 UV, float2 Center, float Rotation)
+				{
+					Rotation = Rotation * (3.1415926f / 180.0f);
+					UV -= Center;
+					float s = sin(Rotation);
+					float c = cos(Rotation);
+					float2x2 rMatrix = float2x2(c, -s, s, c);
+					rMatrix *= 0.5;
+					rMatrix += 0.5;
+					rMatrix = rMatrix * 2 - 1;
+					UV.xy = mul(UV.xy, rMatrix);
+					UV += Center;
+					return UV;
+				}
+
+				fixed4 frag(v2f i) : SV_Target
+				{
+					float2 uv = i.uv;
+					fixed4 col = tex2D(_MainTex, uv);
+					uv = Unity_Rotate_Degrees_float(uv, float2(0.5f, 0.5f), _AngleDeg);
+					fixed4 col2 = tex2D(_SecondTex, uv);
+					return col * col2;
+				}
+			ENDCG
+		}
+		}
+}
